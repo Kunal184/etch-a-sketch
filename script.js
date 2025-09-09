f309018410171cac53ef38ab8e9571c2e1a97ac0
Kunal184 <184780740+Kunal184@users.noreply.github.com>
2025-09-09 21:45:30 +0000

    fixed etch-a-sketch logic

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const container = document.querySelector(".container");
 
+
 for(let i = 0; i < 256; i++){
     const square = document.createElement("div");
     square.classList.add("square")
     container.appendChild(square);
+    square.addEventListener("mouseenter", () => {square.style.backgroundColor = "pink"});
 }
+
+const body = document.querySelector("body");
+const button = document.createElement("button");
+body.insertBefore(button, container);
+button.textContent = "Change layout";
+
+
+function changeLayout(){
+let n = Number(prompt("Enter new layout (axa): "));
+container.innerHTML = '';
+for (let i = 0; i < n * n; i++) {
+     const square = document.createElement("div");
+     square.classList.add("square");
+
+     const squareSize = 640/n;
+     square.style.width = `${squareSize}px`;
+     square.style.height = `${squareSize}px`
+     square.addEventListener("mouseenter", () => {square.style.backgroundColor = "pink"});
+     container.appendChild(square);
+}
+
+}
+
+button.addEventListener("click", changeLayout);
 
